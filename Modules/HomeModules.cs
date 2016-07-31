fb8f115860e5b2d10245abd40cc803055a37bd89
Russ Davies <russdavies392@gmail.com>
2016-07-31 11:52:54 -0700

    add shell of Modules and Views directories

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,15 @@
+using Nancy;
+using System.Collections.Generic;
+
+namespace HairSalon
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
